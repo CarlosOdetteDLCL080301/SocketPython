5d9ba567cf3aac8b32f9eb70b3f9933e387bfb62
CarlosOdetteDLCL080301 <carlosodettedlcl@gmail.com>
2023-10-22 14:43:40 -0600

    Comunicación Interna Correcta

--- cliente.py
+++ cliente.py
@@ -9,37 +9,37 @@ import time
 
 
 # Función para enviar mensajes al servidor
-def send_message(client_socket, message):
+def enviar_mensaje(socketCliente, mensaje):
     # Obtiene la marca de tiempo actual del cliente
-    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    tiempoProcesado = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     
     # Construye el mensaje concatenando la marca de tiempo con el mensaje original
-    full_message = f"[{timestamp}] {message}"
+    mensajeCompleto = f"[{tiempoProcesado}] {mensaje}"
     
     # Envia el mensaje al servidor como bytes codificados en UTF-8
-    client_socket.send(full_message.encode('utf-8'))
+    socketCliente.send(mensajeCompleto.encode('utf-8'))
 
 
 def main():
     # Crea un nuevo socket del cliente
-    mi_socket = socket.socket()
+    miSocket = socket.socket()
     
     # Establece una conexión con el servidor en localhost y el puerto 8000
-    mi_socket.connect(('localhost', 8000))
+    miSocket.connect(('localhost', 8000))
 
     while True:
         # Obtiene un mensaje ingresado por el usuario
-        message = input("Escribe un mensaje: ")
+        mensaje = input("Escribe un mensaje: ")
         
-        # Llama a la función 'send_message' para enviar el mensaje al servidor
-        send_message(mi_socket, message)
+        # Llama a la función 'enviar_mensaje' para enviar el mensaje al servidor
+        enviar_mensaje(miSocket, mensaje)
 
         # Recibe una respuesta del servidor (hasta 1024 bytes) y la decodifica
-        response = mi_socket.recv(1024)
-        print(response.decode('utf-8'))
+        respuesta = miSocket.recv(1024)
+        print(respuesta.decode('utf-8'))
 
     # Cierra la conexión con el servidor
-    mi_socket.close()
+    miSocket.close()
 
 
 if __name__ == "__main__":
