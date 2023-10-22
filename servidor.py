76195c6e076a54ef14d7bce26d2994d299660fc1
CarlosOdetteDLCL080301 <carlosodettedlcl@gmail.com>
2023-10-22 14:54:48 -0600

    Se crea servidor, en la PC anfitrion

--- servidor.py
+++ servidor.py
@@ -5,62 +5,62 @@ import time
 # Diccionario para almacenar los mensajes de los clientes
 mensajes = {}
 
-def handle_client(client_socket, client_address):
+def procesarCliente(socketCliente, ipCliente):
     while True:
         try:
             # Recibe datos enviados por el cliente (hasta 1024 bytes)
-            data = client_socket.recv(1024)
+            datos = socketCliente.recv(1024)
             
             # Si no se recibe ningún dato, se sale del bucle
-            if not data:
+            if not datos:
                 break
 
             # Decodifica los datos recibidos en formato UTF-8 para obtener el mensaje
-            message = data.decode('utf-8')
+            mensaje = datos.decode('utf-8')
             
             # Obtiene la marca de tiempo actual del servidor
-            timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            tiempoProcesado = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             
             # Muestra el mensaje del cliente junto con la marca de tiempo y su dirección
-            print(f"Mensaje de {client_address} ({timestamp}): {message}")
+            print(f"Mensaje de {ipCliente} ({tiempoProcesado}): {mensaje}")
 
             # Almacena el mensaje en el diccionario de mensajes
-            if client_address not in mensajes:
-                mensajes[client_address] = []
-            mensajes[client_address].append((timestamp, message))
+            if ipCliente not in mensajes:
+                mensajes[ipCliente] = []
+            mensajes[ipCliente].append((tiempoProcesado, mensaje))
 
             # Envia una respuesta al cliente confirmando la recepción del mensaje
-            response = f"Mensaje recibido de {client_address}"
-            client_socket.send(response.encode('utf-8'))
+            respuesta = f"Mensaje recibido de {ipCliente}"
+            socketCliente.send(respuesta.encode('utf-8'))
 
-        except Exception as e:
+        except Exception as error:
             # Si ocurre un error, se muestra en la consola y se sale del bucle
-            print(f"Ocurrió un error: {e}")
+            print(f"Ocurrió un error: {error}")
             break
 
     # Cierra la conexión con el cliente una vez que se completa la comunicación
-    client_socket.close()
+    socketCliente.close()
 
 
 def main():
     # Crea un socket de tipo AF_INET (IPv4) y SOCK_STREAM (TCP)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Enlaza el socket al localhost en el puerto 8000
-    server_socket.bind(('localhost', 8000))
+    socketServer.bind(('localhost', 8000))
 
     # Escucha hasta 5 conexiones entrantes en el socket
-    server_socket.listen(5)
+    socketServer.listen(5)
     print("Servidor esperando conexiones...")
 
     while True:
         # Acepta una conexión entrante y obtiene el socket del cliente y su dirección
-        client_socket, client_address = server_socket.accept()
-        print(f"Nueva conexión establecida con {client_address}")
+        socketCliente, ipCliente = socketServer.accept()
+        print(f"Nueva conexión establecida con {ipCliente}")
 
         # Inicia un subproceso para manejar al cliente
-        client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
-        client_handler.start()
+        manejandoCliente = threading.Thread(target=procesarCliente, args=(socketCliente, ipCliente))
+        manejandoCliente.start()
 
 
 if __name__ == "__main__":
